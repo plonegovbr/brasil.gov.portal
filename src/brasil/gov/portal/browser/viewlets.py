2252418a36c4f9adf7020a93e0ed1fdfbfe56d4e
Érico Andrei <erico@simplesconsultoria.com.br>
2013-05-10 17:33:28 -0300

    Adiciona viewlet de listagem de destaques

--- src/brasil/gov/portal/browser/viewlets.py
+++ src/brasil/gov/portal/browser/viewlets.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 """ Modulo que implementa os viewlets do Portal Modelo"""
 from brasil.gov.portal.config import REDES
+from collective.cover.content import ICover
+from five import grok
 from plone.app.layout.viewlets import ViewletBase
+from plone.app.layout.viewlets.interfaces import IPortalHeader
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getMultiAdapter
+from zope.interface import Interface
+
+
+FEATURES_ID = 'destaques'
+grok.templatedir('templates')
 
 
 class ServicosViewlet(ViewletBase):
@@ -69,3 +78,35 @@ class RedesSociaisViewlet(ViewletBase):
 
     def available(self):
         return self.redes and True or False
+
+
+class Destaques_Viewlet(grok.Viewlet):
+    """Viewlet que lista destaques do site
+    """
+
+    grok.viewletmanager(IPortalHeader)
+    grok.context(Interface)
+    grok.order(100)
+
+    def editable(self):
+        ''' Validamos se o destaques eh editavel
+        '''
+        destaques = getattr(self.portal, FEATURES_ID, None)
+        if destaques:
+            context_state = getMultiAdapter((destaques, self.request),
+                                            name=u'plone_context_state')
+            return context_state.is_editable()
+
+    def available(self):
+        ''' Exibiremos ou nao este viewlet
+        '''
+        context = self.context
+        portal_state = getMultiAdapter((context, self.request),
+                                       name=u'plone_portal_state')
+        self.portal = portal_state.portal()
+        self.portal_url = portal_state.portal_url()
+        self.destaques = getattr(self.portal, FEATURES_ID, None)
+        if self.destaques:
+            return ICover.providedBy(self.destaques)
+        else:
+            return False
