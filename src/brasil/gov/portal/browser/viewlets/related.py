3c95c00c36948e58f2835ef83d28745bb37c2205
Érico Andrei <erico@simplesconsultoria.com.br>
2013-05-21 17:20:01 -0300

    Adiciona viewlet de itens relacionados especifico para tipos dexterity

---
+++ src/brasil/gov/portal/browser/viewlets/related.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+""" Modulo que implementa o viewlet de conteudo relacionados do Portal """
+from Acquisition import aq_inner
+from five import grok
+from plone.app.layout.viewlets.interfaces import IBelowContentBody
+from plone.app.relationfield.behavior import IRelatedItems
+from plone.dexterity.interfaces import IDexterityContent
+
+
+grok.templatedir('templates')
+
+
+class RelatedItemsViewlet(grok.Viewlet):
+    """Viewlet de itens relacionados para Dexterity
+    """
+
+    grok.viewletmanager(IBelowContentBody)
+    grok.context(IDexterityContent)
+    grok.order(100)
+
+    def related(self):
+        context = aq_inner(self.context)
+        res = ()
+        try:
+            behavior = IRelatedItems(context)
+        except TypeError:
+            return res
+        tools = context.restrictedTraverse('@@plone_tools')
+        catalog = tools.catalog()
+        related = behavior.relatedItems
+        if related:
+            related = [item.to_path for item in related]
+            brains = catalog(path=related)
+            if brains:
+                # build a position dict by iterating over the items once
+                positions = dict([(v, i) for (i, v) in enumerate(related)])
+                # We need to keep the ordering intact
+                res = list(brains)
+
+                def _key(brain):
+                    return positions.get(brain.UID, -1)
+                res.sort(key=_key)
+        return res
