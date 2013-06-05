1129120abc3f9c11f05f86a05828e863cf0d3bd8
Érico Andrei <erico@simplesconsultoria.com.br>
2013-06-05 00:06:48 -0300

    Sobreescreve a view padrao do embedder para acertar a ordem dos viewlets

---
+++ src/brasil/gov/portal/browser/content/embedder.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+from five import grok
+from sc.embedder.content.embedder import IEmbedder
+
+grok.templatedir('templates')
+
+
+class View(grok.View):
+    grok.context(IEmbedder)
+    grok.require('zope2.View')
+    grok.name('view')
+
+    def get_player_pos_class(self):
+        """ Returns the css class based on the position of the embed item.
+        """
+        pos = self.context.player_position
+        css_class = "%s_embedded" % pos.lower()
+        return css_class
