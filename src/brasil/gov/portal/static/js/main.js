397cedcf85fa8a5c94fcb4d2f19eaf5902ad9e34
Felipe Duardo <felipeduardo@gmail.com>
2013-10-18 11:19:43 -0300

    Alterado sintaxe para conotação json e use strict

--- src/brasil/gov/portal/static/js/main.js
+++ src/brasil/gov/portal/static/js/main.js
@@ -1,116 +1,140 @@
-jQuery(document).ready(function ($) {
+var PBrasil = {
+    init: function(){
+        this.main();
+        this.addClassHtml();
+        this.onclickBuscar();
+        this.bugfixBase();
+    },
 
-    // simulando click no botao do portlet header via mobile
-    // author: deserto digital
-    $('.portletNavigationTree .portletHeader').click(function () {
-        $(this).toggleClass('ativo');
-        $(this).next().slideToggle();
-    });
+    main: function() {
 
-    var menuTrigger = $(".menuTrigger");
-    var navigationEl = $("#navigation");
+        // simulando click no botao do portlet header via mobile
+        // author: deserto digital
+        $('.portletNavigationTree .portletHeader').click(function () {
+            $(this).toggleClass('ativo');
+            $(this).next().slideToggle();
+        });
 
-    menuTrigger.click(function (e) {
-        e.preventDefault();
-        navigationEl.toggleClass("ativo");
-    });
+        var menuTrigger = $(".menuTrigger");
+        var navigationEl = $("#navigation");
 
-    var sectionTrigger = $(".mobile .portletNavigationTree dt a");
-    sectionTrigger.append("<span></span>");
+        menuTrigger.click(function (e) {
+            e.preventDefault();
+            navigationEl.toggleClass("ativo");
+        });
+
+        var sectionTrigger = $(".mobile .portletNavigationTree dt a");
+        sectionTrigger.append("<span></span>");
 
 
-    sectionTrigger.click(function (e) {
-        e.preventDefault();
-        $(this).parent().parent().toggleClass("ativo");
-    });
-    $("ul li:last-child").addClass("last-item");
+        sectionTrigger.click(function (e) {
+            e.preventDefault();
+            $(this).parent().parent().toggleClass("ativo");
+        });
+        $("ul li:last-child").addClass("last-item");
 
-    var responsiveResize, root;
+        var responsiveResize, root;
 
-    root = typeof exports !== "undefined" && exports !== null ? exports : this;
+        root = typeof exports !== "undefined" && exports !== null ? exports : this;
 
-    root.ResponsiveResize = function () {
-        var _Singleton, _base;
-        _Singleton = (function () {
-            function _Singleton() {}
+        root.ResponsiveResize = function () {
+            var _Singleton, _base;
+            _Singleton = (function () {
+                function _Singleton() {}
 
-            _Singleton.prototype.perspectiva_anterior = '';
-            _Singleton.prototype.scrollbar = false;
+                _Singleton.prototype.perspectiva_anterior = '';
+                _Singleton.prototype.scrollbar = false;
 
-            _Singleton.prototype.resize = function () {
-                var perspectiva_atual;
+                _Singleton.prototype.resize = function () {
+                    var perspectiva_atual;
 
-                if ($(window).width() <= 480) {
-                    perspectiva_atual = 'mobile';
-                } else {
-                    perspectiva_atual = 'desktop';
-                }
-
-                if (this.perspectiva_anterior !== perspectiva_atual) {
-                    this.perspectiva_anterior = perspectiva_atual;
-
-                    if (perspectiva_atual === 'mobile') {
-                        $("body").addClass("mobile");
-                    } else { // Desktop
-                        $("body").removeClass("mobile");
+                    if ($(window).width() <= 480) {
+                        perspectiva_atual = 'mobile';
+                    } else {
+                        perspectiva_atual = 'desktop';
                     }
-                }
-            };
 
-            return _Singleton;
-        })();
+                    if (this.perspectiva_anterior !== perspectiva_atual) {
+                        this.perspectiva_anterior = perspectiva_atual;
+
+                        if (perspectiva_atual === 'mobile') {
+                            $("body").addClass("mobile");
+                        } else { // Desktop
+                            $("body").removeClass("mobile");
+                        }
+                    }
+                };
+
+                return _Singleton;
+            })();
 
 
-        if ((_base = root.ResponsiveResize).instance == null) {
-            _base.instance = new _Singleton();
+            if ((_base = root.ResponsiveResize).instance == null) {
+                _base.instance = new _Singleton();
+            }
+            return root.ResponsiveResize.instance;
+        };
+
+        var resize = function () {
+            responsiveResize = new root.ResponsiveResize();
+            responsiveResize.resize();
         }
-        return root.ResponsiveResize.instance;
-    };
 
-    var resize = function () {
-        responsiveResize = new root.ResponsiveResize();
-        responsiveResize.resize();
-    }
+        $(window).resize(function () {
+            resize();
+        });
 
-    $(window).resize(function () {
         resize();
-    });
 
-    resize();
+        $('.newsImageContainer .photo-icon').on('click', function(e) {
+            e.preventDefault();
+            //$(this).parent().find('.carrossel-images').slideToggle();
+            $('#parent-fieldname-image').trigger('click');
+        });
 
-    $('.newsImageContainer .photo-icon').on('click', function(e) {
-        e.preventDefault();
-        //$(this).parent().find('.carrossel-images').slideToggle();
-        $('#parent-fieldname-image').trigger('click');
-    });
+    },
 
-    $('#link-buscar').click(function (e) {
-        e.preventDefault();
-        window.location.hash = '#portal-searchbox';
-        $('.searchField').focus();
-    });
+    addClassHtml: function(){
 
-    $("#portal-column-one div:first-child").addClass("first-item");
+        $("#portal-column-one div:first-child").addClass("first-item");
 
-    /* Protection from the Content Manager */
-    var firstNavigation = $("#portal-column-one .portletWrapper .portletNavigationTree")[0];
-    if(firstNavigation){
-        $(firstNavigation).addClass("first-item-nav");
-    }
+        /* Protection from the Content Manager */
+        var firstNavigation = $("#portal-column-one .portletWrapper .portletNavigationTree")[0];
+        if(firstNavigation){
+            $(firstNavigation).addClass("first-item-nav");
+        }
 
-    if($('.link-externo').length > 0){
-        $('.link-externo .collection-item:even').addClass ('even');
-        $('.link-externo .collection-item:odd').addClass ('odd');
-    }
+        if($('.link-externo').length > 0){
+            $('.link-externo .collection-item:even').addClass ('even');
+            $('.link-externo .collection-item:odd').addClass ('odd');
+        }
+
+    },
+
+    onclickBuscar: function(){
+
+        $('#link-buscar').click(function (e) {
+            e.preventDefault();
+            window.location.hash = '#portal-searchbox';
+            $('.searchField').focus();
+        });
+
+    },
 
     /*
      * Bug fix para o bug de <base url=""> do Plone
      */
+    bugfixBase: function(){
 
-    if($("base").length > 0 && $(".userrole-anonymous").length > 0) {
-        var aCurrentUrl = document.location.href.match(/(^[^#]*)/);
+        if($("base").length > 0 && $(".userrole-anonymous").length > 0) {
+            var aCurrentUrl = document.location.href.match(/(^[^#]*)/);
 
-        $("base").attr("href", aCurrentUrl[1]);
+            $("base").attr("href", aCurrentUrl[1]);
+        }
     }
+}
 
+jQuery(document).ready(function ($) {
+    "use strict";
+    PBrasil.init();
 });
