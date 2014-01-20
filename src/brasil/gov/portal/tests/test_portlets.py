b8b2e5a41c3ccbeb502424338febaea8d754668d
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2014-01-20 09:46:51 -0200

    Fixed tests

--- src/brasil/gov/portal/tests/test_portlets.py
+++ src/brasil/gov/portal/tests/test_portlets.py
@@ -1,5 +1,6 @@
-from brasil.gov.portal.browser.portlets import navigation
 from brasil.gov.portal.testing import INTEGRATION_TESTING
+from plone import api
+from plone.app.portlets.portlets import navigation
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.portlets.interfaces import IPortletManager
@@ -27,7 +28,7 @@ class NavigationTestCase(unittest.TestCase):
                  manager=None,
                  assignment=None):
         context = context or self.portal
-        request = request or self.app.REQUEST
+        request = request or self.request
         view = view or self.portal.restrictedTraverse('@@plone')
         manager = manager or getUtility(IPortletManager,
                                         name='plone.leftcolumn',
@@ -42,34 +43,30 @@ class NavigationTestCase(unittest.TestCase):
                                IPortletRenderer)
 
     def populateSite(self):
-        self.setRoles(['Manager'])
-        if 'Members' in self.portal:
-            self.portal._delObject('Members')
-            self.folder = None
-        if 'news' in self.portal:
-            self.portal._delObject('news')
-        if 'events' in self.portal:
-            self.portal._delObject('events')
-        if 'front-page' in self.portal:
-            self.portal._delObject('front-page')
-        self.portal.invokeFactory('Document', 'doc1')
-        self.portal.invokeFactory('Document', 'doc2')
-        self.portal.invokeFactory('Document', 'doc3')
-        self.portal.invokeFactory('Folder', 'folder1')
-        self.portal.invokeFactory('Link', 'link1')
-        self.portal.link1.setRemoteUrl('http://plone.org')
-        self.portal.link1.reindexObject()
-        folder1 = getattr(self.portal, 'folder1')
-        folder1.invokeFactory('Document', 'doc11')
-        folder1.invokeFactory('Document', 'doc12')
-        folder1.invokeFactory('Document', 'doc13')
-        self.portal.invokeFactory('Folder', 'folder2')
-        folder2 = getattr(self.portal, 'folder2')
-        folder2.invokeFactory('Document', 'doc21')
-        folder2.invokeFactory('Document', 'doc22')
-        folder2.invokeFactory('Document', 'doc23')
-        folder2.invokeFactory('File', 'file21')
-        self.setRoles(['Member'])
+        with api.env.adopt_roles(['Manager']):
+            if 'Members' in self.portal:
+                self.portal._delObject('Members')
+                self.folder = None
+            if 'news' in self.portal:
+                self.portal._delObject('news')
+            if 'events' in self.portal:
+                self.portal._delObject('events')
+            if 'front-page' in self.portal:
+                self.portal._delObject('front-page')
+            self.portal.invokeFactory('Document', 'doc1')
+            self.portal.invokeFactory('Document', 'doc2')
+            self.portal.invokeFactory('Document', 'doc3')
+            self.portal.invokeFactory('Folder', 'folder1')
+            folder1 = getattr(self.portal, 'folder1')
+            folder1.invokeFactory('Document', 'doc11')
+            folder1.invokeFactory('Document', 'doc12')
+            folder1.invokeFactory('Document', 'doc13')
+            self.portal.invokeFactory('Folder', 'folder2')
+            folder2 = getattr(self.portal, 'folder2')
+            folder2.invokeFactory('Document', 'doc21')
+            folder2.invokeFactory('Document', 'doc22')
+            folder2.invokeFactory('Document', 'doc23')
+            folder2.invokeFactory('File', 'file21')
 
     def testRootItensOrder(self):
         viewRoot = self.renderer(self.portal)
