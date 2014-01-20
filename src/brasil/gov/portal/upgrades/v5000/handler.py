f71be52bdd2da99eb76cb53b997b33461817c40c
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2014-01-20 15:14:53 -0200

    Adicionada visualização de albuns para site padrão

---
+++ src/brasil/gov/portal/upgrades/v5000/handler.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+from brasil.gov.portal.config import PROJECTNAME
+from plone.app.upgrade.utils import loadMigrationProfile
+
+import logging
+
+
+def apply_profile(context):
+    ''' Atualiza perfil para versao 5000 '''
+    logger = logging.getLogger(PROJECTNAME)
+    profile = 'profile-brasil.gov.portal.upgrades.v5000:default'
+    loadMigrationProfile(context, profile)
+    logger.info('Atualizado para versao 5000')
