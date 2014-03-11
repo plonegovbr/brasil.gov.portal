06374ceac5a407088d8cd29ee576c79f6469bcc5
Érico Andrei <erico@simplesconsultoria.com.br>
2014-03-11 11:55:33 -0300

    Back to development: 1.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0.2.1'
+version = '1.0.3.dev0'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CONTRIBUTORS.rst').read() + '\n' +
