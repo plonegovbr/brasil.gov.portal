3d40d2b1c826cf34de0ee088e228c6a2b6f214fc
Érico Andrei <erico@simplesconsultoria.com.br>
2014-06-05 20:05:35 -0300

    Separa view da classe de conteudo para o tipo ExternalContent

---
+++ src/brasil/gov/portal/browser/content/external.py
@@ -0,0 +1,23 @@
+    # -*- coding: utf-8 -*-
+from five import grok
+from brasil.gov.portal.content.external import IExternalContent
+from plone import api
+
+
+grok.templatedir('templates')
+
+
+class ExternalContentView(grok.View):
+    grok.context(IExternalContent)
+    grok.name('view')
+
+    def update(self):
+        """Redireciona usuarios sem poder de edicao para o conteudo externo"""
+        context = self.context
+        mtool = api.portal.get_tool('portal_membership')
+
+        can_edit = mtool.checkPermission('Modify portal content', context)
+
+        if not can_edit:
+            url = context.remoteUrl
+            return context.REQUEST.RESPONSE.redirect(url)
--- src/brasil/gov/portal/content/external.py
+++ src/brasil/gov/portal/content/external.py
@@ -1,12 +1,7 @@
     # -*- coding: utf-8 -*-
-from five import grok
 from plone.dexterity.content import Item
-from Products.CMFCore.utils import getToolByName
 from zope.interface import implements
 from zope.interface import Interface
-
-
-grok.templatedir('templates')
 
 
 def has_image(blog_entry):
@@ -39,19 +34,3 @@ class ExternalContent(Item):
                         scale=scale,
                         css_class=css_class,
                         **kw)
-
-
-class ExternalContentView(grok.View):
-    grok.context(IExternalContent)
-    grok.name('view')
-
-    def update(self):
-        """Redireciona usuarios sem poder de edicao para o conteudo externo"""
-        context = self.context
-        mtool = getToolByName(context, 'portal_membership')
-
-        can_edit = mtool.checkPermission('Modify portal content', context)
-
-        if not can_edit:
-            url = context.remoteUrl
-            return context.REQUEST.RESPONSE.redirect(url)
