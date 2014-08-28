233231ae13e794120859225cbd6b744576bdc495
Danilo Barbato <dbarbato@tv1.com.br>
2014-08-28 13:00:55 -0300

    Ajusta bug de coleção

--- src/brasil/gov/portal/static/js/main.js
+++ src/brasil/gov/portal/static/js/main.js
@@ -5,6 +5,7 @@ var PBrasil = {
         this.onclickBuscar();
         this.bugfixBase();
         this.carregaDestaques();
+        this.bugfixQuerywidget();
     },
 
     main: function() {
@@ -119,6 +120,34 @@ var PBrasil = {
             $('.searchField').focus();
         });
 
+    },
+
+    /*
+     * Bug fix para o bug de plone.formwidget.querystring versao acima de 1.0b3
+     */
+    bugfixQuerywidget: function(){
+        if ($(".QueryWidget").length === 0) {
+            return false;
+        }
+        $.querywidget.init();
+        $('#sort_on').val($('#form-widgets-sort_on').val());
+        if ($('#form-widgets-sort_reversed-0').attr('checked')) {
+            $('#sort_order').attr('checked', true);
+        } else {
+            $('#sort_order').attr('checked', false);
+        }
+        $("#sort_on").live('click', function () {
+            $('#form-widgets-sort_on').val($(this).val());
+        });
+        $("#sort_order").live('click', function () {
+            if ($(this).is(":checked")) {
+                $('#form-widgets-sort_reversed-0').attr('checked', true);
+            } else {
+                $('#form-widgets-sort_reversed-0').attr('checked', false);
+            }
+        });
+        $('#formfield-form-widgets-sort_on').hide();
+        $('#formfield-form-widgets-sort_reversed').hide();
     },
 
     /*
