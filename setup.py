9bed1d8a18299da3b178f538c5ddd8e75b914195
hvelarde <hector.velarde@gmail.com>
2015-09-30 16:26:46 -0300

    Back to development: 1.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.1.3'
+version = '1.1.4.dev0'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CONTRIBUTORS.rst').read() + '\n' +
