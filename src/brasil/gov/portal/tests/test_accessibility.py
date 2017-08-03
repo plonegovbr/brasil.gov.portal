7385e163862d16986a2f34ac05ef159967c319cd
idgserpro <idg@serpro.gov.br>
2017-08-03 10:09:50 -0300

    elemento <a> com ausência de texto descritivo ajuste dos links vazios acontent, anavigation e afooter para atender os critérios de acessibilidade

--- src/brasil/gov/portal/tests/test_accessibility.py
+++ src/brasil/gov/portal/tests/test_accessibility.py
@@ -11,8 +11,12 @@ from plone.app.testing import setRoles
 from plone.app.theming.utils import applyTheme
 from plone.app.theming.utils import getTheme
 from plone.testing.z2 import Browser
+from Products.Five.browser import BrowserView as View
+from zope.component import queryMultiAdapter
 from zope.interface import alsoProvides
+from zope.viewlet.interfaces import IViewletManager
 
+import transaction
 import unittest
 
 
@@ -29,11 +33,19 @@ class AccessibilityTestCase(unittest.TestCase):
         self.request = self.layer['request']
         self.adapter = SiteControlPanelAdapter(self.portal)
         self.browser = Browser(self.layer['app'])
+        transaction.begin()
+        ltool = self.portal.portal_languages
+        supportedLanguages = ['en', 'es', 'pt-br']
+        ltool.manage_setLanguageSettings('pt-br', supportedLanguages,
+                                         setUseCombinedLanguageCodes=True)
+        transaction.commit()
 
 
 class PortalLogoTestCase(AccessibilityTestCase):
+
     def base_test(self, cor):
-        """Teste base dos temas"""
+        """ Teste base dos temas
+        """
         adapter = self.adapter
         adapter.site_title_1 = u'Portal'
         adapter.site_title_2 = u'Brasil'
@@ -62,6 +74,132 @@ class PortalLogoTestCase(AccessibilityTestCase):
         self.base_test('verde')
 
 
+class AcessibilidadeViewletTestCase(AccessibilityTestCase):
+
+    def test_viewlet_is_present(self):
+        """ Testa se a viewlet foi registrada corretamente
+        """
+        request = self.request
+        context = self.portal
+
+        view = View(context, request)
+
+        manager_name = 'plone.portaltop'
+
+        manager = queryMultiAdapter((context, request, view), IViewletManager, manager_name, default=None)
+
+        # Testa se o viewlet manager existe
+        self.assertIsNotNone(manager)
+
+        manager.update()
+
+        my_viewlet = [v for v in manager.viewlets if v.__name__ == 'brasil.gov.portal.acessibilidade']
+
+        # Testa se o viewlet existe
+        self.assertEqual(len(my_viewlet), 1)
+
+    # FIXME: o teste só irá funcionar com a versão do brasil.gov.temas que implementa
+    # o replace dos itens acontent, anavigation e afooter na regra de cada tema
+    @unittest.expectedFailure
+    def base_test(self, cor, defaultLanguage):
+        """Teste base dos temas"""
+        transaction.begin()
+
+        ltool = self.portal.portal_languages
+        supportedLanguages = ['en', 'es', 'pt-br']
+        ltool.manage_setLanguageSettings(defaultLanguage, supportedLanguages,
+                                         setUseCombinedLanguageCodes=True)
+
+        transaction.commit()
+
+        theme = getTheme(cor)
+        applyTheme(theme)
+
+        self.browser.open(self.portal.absolute_url())
+        contents = self.browser.contents.decode('utf-8')
+
+        if defaultLanguage == 'en':
+            # Testa se a âncora para o conteúdo aparece.
+            self.assertIn(
+                u'<a name="acontent" id="acontent" class="anchor">content</a>',
+                contents
+            )
+            # Testa se a âncora para o menu aparece.
+            self.assertIn(
+                u'<a name="anavigation" id="anavigation" class="anchor">navigation</a>',
+                contents
+            )
+            # Testa se a âncora para o rodapé aparece.
+            self.assertIn(
+                u'<a name="afooter" id="afooter" class="anchor">footer</a>',
+                contents
+            )
+        elif defaultLanguage == 'es':
+            # Testa se a âncora para o conteúdo aparece.
+            self.assertIn(
+                u'<a name="acontent" id="acontent" class="anchor">contenido</a>',
+                contents
+            )
+            # Testa se a âncora para o menu aparece.
+            self.assertIn(
+                u'<a name="anavigation" id="anavigation" class="anchor">navegación</a>',
+                contents
+            )
+            # Testa se a âncora para o rodapé aparece.
+            self.assertIn(
+                u'<a name="afooter" id="afooter" class="anchor">pie de página</a>',
+                contents
+            )
+        elif defaultLanguage == 'pt-br':
+            # Testa se a âncora para o conteúdo aparece.
+            self.assertIn(
+                u'<a name="acontent" id="acontent" class="anchor">conteúdo</a>',
+                contents
+            )
+            # Testa se a âncora para o menu aparece.
+            self.assertIn(
+                u'<a name="anavigation" id="anavigation" class="anchor">menu</a>',
+                contents
+            )
+            # Testa se a âncora para o rodapé aparece.
+            self.assertIn(
+                u'<a name="afooter" id="afooter" class="anchor">rodapé</a>',
+                contents
+            )
+
+    # FIXME: o teste só irá funcionar com a versão do brasil.gov.temas que implementa
+    # o replace dos itens acontent, anavigation e afooter na regra de cada tema
+    @unittest.expectedFailure
+    def test_tema_amarelo(self):
+        self.base_test('amarelo', 'en')
+        self.base_test('amarelo', 'es')
+        self.base_test('amarelo', 'pt-bt')
+
+    # FIXME: o teste só irá funcionar com a versão do brasil.gov.temas que implementa
+    # o replace dos itens acontent, anavigation e afooter na regra de cada tema
+    @unittest.expectedFailure
+    def test_tema_azul(self):
+        self.base_test('azul', 'en')
+        self.base_test('azul', 'es')
+        self.base_test('azul', 'pt-br')
+
+    # FIXME: o teste só irá funcionar com a versão do brasil.gov.temas que implementa
+    # o replace dos itens acontent, anavigation e afooter na regra de cada tema
+    @unittest.expectedFailure
+    def test_tema_branco(self):
+        self.base_test('branco', 'en')
+        self.base_test('branco', 'es')
+        self.base_test('branco', 'pt-br')
+
+    # FIXME: o teste só irá funcionar com a versão do brasil.gov.temas que implementa
+    # o replace dos itens acontent, anavigation e afooter na regra de cada tema
+    @unittest.expectedFailure
+    def test_tema_verde(self):
+        self.base_test('verde', 'en')
+        self.base_test('verde', 'es')
+        self.base_test('verde', 'pt-br')
+
+
 class SiteActionsViewletTestCase(AccessibilityTestCase):
 
     def setUp(self):
@@ -75,10 +213,10 @@ class SiteActionsViewletTestCase(AccessibilityTestCase):
         # Testa se os links foram gerados sem o atributo title.
         url_portal = self.portal.absolute_url()
         self.browser.open(url_portal)
-        self.assertIn('<a href="{0}/acessibilidade" accesskey="5">Acessibilidade</a>'.format(
+        self.assertIn('<a href="{0}/acessibilidade" accesskey="5">'.format(
                       url_portal), self.browser.contents)
-        self.assertIn('<a href="#" accesskey="6">Alto Contraste</a>', self.browser.contents)
-        self.assertIn('<a href="{0}/mapadosite" accesskey="7">Mapa do Site</a>'.format(
+        self.assertIn('<a href="#" accesskey="6">', self.browser.contents)
+        self.assertIn('<a href="{0}/mapadosite" accesskey="7">'.format(
                       url_portal), self.browser.contents)
 
 
