ed3f49887aceea64a3ac36ae4468d22810ae73d7
hvelarde <hector.velarde@gmail.com>
2017-10-24 19:14:32 -0200

    Remove Grok from audio subscribers
    Add documentation and tests.

--- src/brasil/gov/portal/content/audio.py
+++ src/brasil/gov/portal/content/audio.py
@@ -1,38 +1,41 @@
 # -*- coding: utf-8 -*-
+"""Audio content type and subscribers associated with it.
+
+An Audio is a container of audio formats: MP3 (MPEGAudioFile) and
+Vorbis (OGGAudioFile) and can only hold one file of each format,
+that's is handled by a couple of subscribers.
+"""
 from brasil.gov.portal.content.audio_file import IMPEGAudioFile
 from brasil.gov.portal.content.audio_file import IOGGAudioFile
-from five import grok
 from plone.dexterity.content import Container
 from zope.interface import Interface
 from zope.interface import implements
-from zope.lifecycleevent.interfaces import IObjectAddedEvent
-from zope.lifecycleevent.interfaces import IObjectRemovedEvent
 
 
 class IAudio(Interface):
-    """ Representa um Audio """
+    """An Audio (in fact a container of audio formats)."""
 
 
 class Audio(Container):
     implements(IAudio)
 
     def return_ogg(self):
+        """Return the Vorbis version of the audio."""
         sources = self.objectValues()
         for source in sources:
             if IOGGAudioFile.providedBy(source):
                 return source
 
     def return_mp3(self):
+        """Return the MP3 version of the audio."""
         sources = self.objectValues()
         for source in sources:
             if IMPEGAudioFile.providedBy(source):
                 return source
 
 
-@grok.subscribe(IObjectAddedEvent)
-def object_added(event, obj=None):
-    if not obj:
-        obj = event.object
+def object_added(obj, event):
+    """Remove further permission to add a file type after adding it."""
     parent = event.newParent
     if IAudio.providedBy(parent):
         if IMPEGAudioFile.providedBy(obj):
@@ -43,10 +46,8 @@ def object_added(event, obj=None):
             parent.manage_permission(permission, roles=[], acquire=0)
 
 
-@grok.subscribe(IObjectRemovedEvent)
-def object_removed(event, obj=None):
-    if not obj:
-        obj = event.object
+def object_removed(obj, event):
+    """Grant permission to add a file type after removing it."""
     parent = event.oldParent
     if IAudio.providedBy(parent):
         if IMPEGAudioFile.providedBy(obj):
