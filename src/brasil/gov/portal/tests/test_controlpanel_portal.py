f18c002b2f7efa5514e29cea02fceed2a63ca74f
hvelarde <hector.velarde@gmail.com>
2017-10-27 15:00:09 -0200

    Clean up and refactor control panel test
    Remove functional test case as is redundant.

--- src/brasil/gov/portal/tests/test_controlpanel_portal.py
+++ src/brasil/gov/portal/tests/test_controlpanel_portal.py
@@ -1,17 +1,10 @@
 # -*- coding: utf-8 -*-
 """Testa o controlpanel de configuração do Portal Padrão."""
-
 from AccessControl import Unauthorized
 from brasil.gov.portal.controlpanel.portal import ISettingsPortal
-from brasil.gov.portal.testing import FUNCTIONAL_TESTING
 from brasil.gov.portal.testing import INTEGRATION_TESTING
 from plone import api
-from plone.app.testing import setRoles
-from plone.app.testing import TEST_USER_ID
-from plone.app.testing import TEST_USER_NAME
-from plone.app.testing import TEST_USER_PASSWORD
 from plone.registry.interfaces import IRegistry
-from plone.testing.z2 import Browser
 from zope.component import getUtility
 
 import unittest
@@ -27,78 +20,50 @@ class ControlPanelTestCase(unittest.TestCase):
         self.portal = self.layer['portal']
         self.controlpanel = api.portal.get_tool('portal_controlpanel')
 
-    def test_controlpanel_instalado(self):
-        """Verifica se o configlet do controlpanel está instalado."""
-        actions = [a.getAction(self)['id']
-                   for a in self.controlpanel.listActions()]
-        self.assertIn('portal', actions, 'Configlet não instalado.')
-
-    def test_controlpanel_view_existe(self):
+    def test_controlpanel_has_view(self):
         """Verifica se a view de configuração existe."""
         request = self.layer['request']
         view = api.content.get_view(u'portal-settings', self.portal, request)
         view = view.__of__(self.portal)
         self.assertTrue(view())
 
-    def test_controlpanel_permissao_view(self):
-        """Testa a permissão da de configuração"""
-        setRoles(self.portal, TEST_USER_ID, ['Contributor'])
+    def test_controlpanel_view_is_protected(self):
+        from plone.app.testing import logout
+        logout()
         with self.assertRaises(Unauthorized):
             self.portal.restrictedTraverse('@@portal-settings')
 
-    def test_controlpanel_registry_esconde_autor(self):
-        """Verifica se o registro esconde_autor existe."""
-        registry = getUtility(IRegistry)
-        settings = registry.forInterface(ISettingsPortal)
-        self.assertTrue(hasattr(settings, 'esconde_autor'))
-        self.assertEqual(settings.esconde_autor, False)
+    def test_controlpanel_installed(self):
+        """Verifica se o configlet do controlpanel está instalado."""
+        actions = [a.getAction(self)['id']
+                   for a in self.controlpanel.listActions()]
+        self.assertIn('portal', actions)
 
-    def test_controlpanel_registry_esconde_data(self):
-        """Verifica se o registro esconde_data existe."""
-        registry = getUtility(IRegistry)
-        settings = registry.forInterface(ISettingsPortal)
-        self.assertTrue(hasattr(settings, 'esconde_data'))
-        self.assertEqual(settings.esconde_data, False)
+    def test_controlpanel_permissions(self):
+        """Testa a permissão de acesso ao configlet."""
+        roles = ['Manager', 'Site Administrator']
+        for r in roles:
+            with api.env.adopt_roles([r]):
+                configlets = self.controlpanel.enumConfiglets(group='Products')
+                configlets = [a['id'] for a in configlets]
+                self.assertIn('portal', configlets, 'configlet not listed for ' + r)
 
 
-class ControlPanelFunctionalTestCase(unittest.TestCase):
-    """Testes funcionais da configuração do Portal Padrão."""
+class RegistryTestCase(unittest.TestCase):
 
-    layer = FUNCTIONAL_TESTING
+    layer = INTEGRATION_TESTING
 
     def setUp(self):
-        """Configura testes funcionais antes de cada teste."""
         self.portal = self.layer['portal']
-        self.portal_url = self.portal.absolute_url()
-        self.browser = Browser(self.layer['app'])
-        self.login_browser()
-        import transaction
-        transaction.commit()
+        self.registry = getUtility(IRegistry)
+        self.settings = self.registry.forInterface(ISettingsPortal)
 
-    def login_browser(self):
-        """Autentica usuário de teste no browser"""
-        setRoles(self.portal, TEST_USER_ID, ['Site Administrator'])
-        self.browser.handleErrors = False
-        basic_auth = 'Basic {0}'.format(
-            '{0}:{1}'.format(TEST_USER_NAME, TEST_USER_PASSWORD)
-        )
-        self.browser.addHeader('Authorization', basic_auth)
+    def test_esconde_autor_record_in_registry(self):
+        """Verifica se o registro esconde_autor existe."""
+        self.assertTrue(hasattr(self.settings, 'esconde_autor'))
+        self.assertFalse(self.settings.esconde_autor)
 
-    def test_configuracao_portal(self):
-        """Testa a configuração do Portal Padrão."""
-        label_autor = u'Esconde autor'
-        label_data = u'Esconde data de publicação'.encode('utf8')
-        self.browser.open('{0}/{1}'.format(self.portal.absolute_url(),
-                                           '@@overview-controlpanel'))
-        self.browser.getLink('.gov.br: Portal Padrão').click()
-        self.assertFalse(self.browser.getControl(label_autor).selected,
-                         'Campo Esconde autor com valor default errado.')
-        self.assertFalse(self.browser.getControl(label_data).selected,
-                         'Campo Esconde data com valor default errado.')
-        self.browser.getControl(label_autor).selected = True
-        self.browser.getControl(label_data).selected = True
-        self.browser.getControl(name='form.buttons.save').click()
-        self.assertTrue(self.browser.getControl(label_autor).selected,
-                        'Esconde autor não foi salvo.')
-        self.assertTrue(self.browser.getControl(label_data).selected,
-                        'Esconde data não foi salvo.')
+    def test_esconde_data_record_in_registry(self):
+        """Verifica se o registro esconde_data existe."""
+        self.assertTrue(hasattr(self.settings, 'esconde_data'))
+        self.assertFalse(self.settings.esconde_data)
