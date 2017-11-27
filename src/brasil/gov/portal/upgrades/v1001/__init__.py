8e445729652dfad02a51f20bb932fe12feee052f
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2017-11-27 16:20:56 -0200

    Move styles to brasil.gov.temas package

--- src/brasil/gov/portal/upgrades/v1001/__init__.py
+++ src/brasil/gov/portal/upgrades/v1001/__init__.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+from brasil.gov.portal.logger import logger
+from plone import api
+
+
+STYLES = [
+    '++resource++brasil.gov.portal/css/main.css',
+    '++resource++brasil.gov.portal/css/main-print.css'
+]
+
+
+def remove_styles(setup_tool):
+    """Remvove CSS from registered resources."""
+    css_tool = api.portal.get_tool('portal_css')
+    for css in STYLES:
+        css_tool.unregisterResource(id=css)
+        assert css not in css_tool.getResourceIds()
+    logger.info('Styles removed')
