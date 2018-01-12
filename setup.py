89d4f6b644e6e82b2864d6dce4c15c0a5fe127ab
hvelarde <hector.velarde@gmail.com>
2018-01-11 23:13:20 -0200

    Back to development: 2.0a3
    [skip ci]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.0a2'
+version = '2.0a3.dev0'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CONTRIBUTORS.rst').read() + '\n' +
