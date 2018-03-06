2122bbbf04570e93d8c51b7d61649c30e81698b7
hvelarde <hector.velarde@gmail.com>
2018-03-06 16:09:13 -0300

    Add viewlet for error reporting

--- src/brasil/gov/portal/browser/viewlets/__init__.py
+++ src/brasil/gov/portal/browser/viewlets/__init__.py
@@ -1,1 +1,25 @@
 # -*- coding: utf-8 -*-
+from plone import api
+from plone.app.layout.viewlets import ViewletBase
+
+
+class ErrorReportingViewlet(ViewletBase):
+    """A viewlet to display a form for error reporting."""
+
+    def update(self):
+        """The viewlet will be available if there is a published
+        contact form in the site root called "Relatar erros".
+        """
+        results = api.content.find(
+            depth=1,  # look only on portal root
+            id='relatar-erros',
+            portal_type='FormFolder',
+            review_state='published',
+        )
+        assert len(results) in (0, 1)
+        self.available = len(results) == 1
+
+    def render(self):
+        if self.available:
+            return self.index()
+        return u''
