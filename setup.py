3e122441e6790dcfd5f44ade9e2ae79974378068
hvelarde <hector.velarde@gmail.com>
2018-06-14 13:48:09 -0300

    Back to development: 1.5.3
    [skip ci]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.5.2'
+version = '1.5.3.dev0'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CONTRIBUTORS.rst').read() + '\n' +
