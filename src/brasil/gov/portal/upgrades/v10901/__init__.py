7aa3400fa8cfb2f7826b3644cd238b5f6b3582ca
hvelarde <hector.velarde@gmail.com>
2018-07-25 22:25:17 -0300

    Fix logger messages

--- src/brasil/gov/portal/upgrades/v10901/__init__.py
+++ src/brasil/gov/portal/upgrades/v10901/__init__.py
@@ -16,14 +16,15 @@ def remove_portlet_assignments(obj):
         manager = getUtility(IPortletManager, name=name)
         mapping = getMultiAdapter((obj, manager), IPortletAssignmentMapping)
         for i in mapping.keys():
-            logger.info('Removing portlet "{0}" from {1}'.format(i, obj))
+            logger.info('Portlet "{0}" removed'.format(i))
             del mapping[i]
 
 
 def remove_root_portlets(setup_tool):
     """Remove portlet assigments at portal root."""
+    logger.info('Removing portlet assigments at portal root')
     remove_portlet_assignments(api.portal.get())
-    logger.info('Portlet assignments removed from portal root')
+    logger.info('Done')
 
 
 def get_valid_objects(**kw):
@@ -64,6 +65,7 @@ def fix_cover_columns(setup_tool):
             new_layout.append(e)
         return new_layout
 
+    logger.info('Fixing column widths on collective.cover objects')
     for obj in get_valid_objects(portal_type='collective.cover.content'):
         try:
             layout = json.loads(obj.cover_layout)
@@ -72,4 +74,4 @@ def fix_cover_columns(setup_tool):
         layout = fix_column_width(layout)
         obj.cover_layout = json.dumps(layout)
 
-    logger.info('Column widths fixed on collective.cover objects')
+    logger.info('Done')
