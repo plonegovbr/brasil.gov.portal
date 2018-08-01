19a7813694f929b5bf800a4a665fa2059842b188
hvelarde <hector.velarde@gmail.com>
2018-08-01 16:01:11 -0300

    Add upgrade step do update collective.cover default settings

--- src/brasil/gov/portal/tests/test_upgrades.py
+++ src/brasil/gov/portal/tests/test_upgrades.py
@@ -210,7 +210,7 @@ class to10901TestCase(UpgradeBaseTestCase):
 
     def test_registered_steps(self):
         steps = len(self.setup.listUpgrades(self.profile_id)[0])
-        self.assertEqual(steps, 3)
+        self.assertEqual(steps, 4)
 
     def test_remove_root_portlets(self):
         title = u'Remove portlet assigments at portal root'
@@ -222,6 +222,14 @@ class to10901TestCase(UpgradeBaseTestCase):
 
     def test_fix_column_widths(self):
         title = u'Fix column widths on cover objects'
+        step = self._get_upgrade_step_by_title(title)
+        self.assertIsNotNone(step)
+
+        # execute upgrade step
+        self._do_upgrade(step)
+
+    def test_update_cover_settings(self):
+        title = u'Update collective.cover default settings'
         step = self._get_upgrade_step_by_title(title)
         self.assertIsNotNone(step)
 
