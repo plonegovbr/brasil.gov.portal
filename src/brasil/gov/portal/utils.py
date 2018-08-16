299478755417fb4fa6fbb5f832530dff385f517c
hvelarde <hector.velarde@gmail.com>
2018-08-16 09:47:53 -0300

    Add validator for backgorund image

--- src/brasil/gov/portal/utils.py
+++ src/brasil/gov/portal/utils.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from brasil.gov.portal import _
+from plone.formwidget.namedfile.converter import b64decode_file
+from plone.namedfile.file import NamedImage
 from six.moves.urllib.parse import urlparse
 from zope.interface import Invalid
 
@@ -8,7 +10,7 @@ MESSAGE = _(u'You must use "Title|http://example.org" format to fill each line.'
 
 
 def validate_list_of_links(value):
-    """Validate value is a list of strings that follow the predefined
+    """Check if value is a list of strings that follow the predefined
     format: "Title|http://example.org".
     """
     if not value:
@@ -24,4 +26,19 @@ def validate_list_of_links(value):
         parsed = urlparse(v.strip())
         if not all([parsed.scheme, parsed.netloc]):
             raise Invalid(MESSAGE)
+    return True
+
+
+def validate_background_image(value):
+    """Check if background image has the right dimensions."""
+    if not value:
+        return True
+
+    filename, data = b64decode_file(value)
+    image = NamedImage(data=data, filename=filename)
+
+    width, height = image.getImageSize()
+    if width != 1440 or height != 605:
+        raise Invalid(_(u'Image should be 1440px width and 605px height.'))
+
     return True
