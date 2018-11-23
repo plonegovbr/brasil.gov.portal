c12dc3aa9e284cae05224ae1b26eed878dc82b1e
hvelarde <hector.velarde@gmail.com>
2018-11-23 15:52:44 -0200

    Fix package classifiers
    [skip ci]

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="Implementação Modelo da Identidade Digital de Governo",
     long_description=long_description,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
